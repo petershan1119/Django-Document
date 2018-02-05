7aa6f4aa74c96fe1dfee9b5c055d2f66e7e13b39
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-05 12:51:42 +0900

    blank, null, choices 적용

---
+++ django/myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Musician, Album, Person
+
+admin.site.register(Musician)
+admin.site.register(Album)
+admin.site.register(Person)
---
+++ django/myapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100, blank=True)
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField(
+        blank=True,
+        null=True,
+    )
+    num_states = models.IntegerField()
+
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(
+        max_length=1,
+        choices=SHIRT_SIZES,
+    )
+
+    def __str__(self):
+        return f'{self.name} (셔츠 사이즈: {self.get_shirt_size_display()})'
