10291335a40e335307b2c011dbc28fb396b3f3ad
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-06 10:56:12 +0900

    many_to_many에서 models 모듈화 및 구분

--- django/many_to_many/models/intermediate.py
+++ django/many_to_many/models/intermediate.py
@@ -2,20 +2,11 @@ from datetime import datetime
 from django.utils import timezone
 from django.db import models
 
-
-class Topping(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Pizza(models.Model):
-    name = models.CharField(max_length=50)
-    toppings = models.ManyToManyField(Topping)
-
-    def __str__(self):
-        return self.name
+__all__ = (
+    'Post',
+    'User',
+    'Postlike',
+)
 
 
 # Extra fields on many-to-many relationships
