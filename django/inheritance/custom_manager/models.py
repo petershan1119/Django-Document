9c608d5db16dfb5da2ec956fd32c582ffc6cde76
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-09 15:06:45 +0900

    custom manager등 연습

--- django/inheritance/custom_manager/models.py
+++ django/inheritance/custom_manager/models.py
@@ -3,11 +3,37 @@ from django.db.models import Manager
 
 
 class CustomManager(Manager):
-    pass
+    def get_queryset(self):
+        print('Custom manager get_queryset!')
+        return super().get_queryset()
 
+
+class OtherManager(Manager):
+    def get_queryset(self):
+        print('Other manager get_queryset!')
+        return super().get_queryset()
 
 class AbstractBase(models.Model):
     objects = CustomManager()
 
     class Meta:
         abstract = True
+
+
+class ChildA(AbstractBase):
+    pass
+
+
+class ChildB(AbstractBase):
+    default_manager = OtherManager()
+
+
+class ExtraManagerModel(models.Model):
+    extra_manager = OtherManager()
+
+    class Meta:
+        abstract = True
+
+
+class ChildC(AbstractBase, ExtraManagerModel):
+    pass
